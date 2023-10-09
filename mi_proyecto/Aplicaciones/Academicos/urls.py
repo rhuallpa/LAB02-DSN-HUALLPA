ec8d04d8fd2838a775a2ebac8787c0fa475ca86e
rhuallpa <rebeca.huallpa@tecsup.edu.pe>
2023-10-08 19:05:52 -0500

    first commit

---
+++ mi_proyecto/Aplicaciones/Academicos/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+
+
+
+    # URLs relacionadas con Persona y Dirección
+    path('persona/', views.persona, name='persona'),
+    path('registrar_persona/', views.registrar_persona, name='registrar_persona'),
+    path('eliminar_persona/<str:id_persona>/', views.eliminar_persona, name='eliminar_persona'),
+    path('editar_persona/<str:id_persona>/', views.editar_persona, name='editar_persona'),
+
+
+
+    # URLs de autenticación
+    path('signup/', views.signup, name='signup'),
+    path('home/', views.homes, name='home'),
+    path('signin/', views.signin, name='signin'),
+    path('signout/', views.signout, name='signout'),
+    path('profile/', views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+]
+
